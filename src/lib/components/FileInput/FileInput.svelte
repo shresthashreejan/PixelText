774e9a23623a11971e6301708bb097bd4a840ac4
shreejan shrestha <shr.shreejan@gmail.com>
2024-07-08 22:08:47 +0545

    Added a button

---
+++ src/lib/components/FileInput/FileInput.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<Button>Upload</Button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import FileInput from '$lib/components/FileInput/FileInput.svelte';
+</script>
+
+<main class="flex min-h-screen items-center justify-center">
+	<FileInput />
+</main>
